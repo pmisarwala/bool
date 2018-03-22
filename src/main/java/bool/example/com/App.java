fb6344ecff6c31eb3a8e91d91c7a7b24f298ad1d
parvez <parvezmisarwala@gmail.com>
2018-03-22 15:53:26 +0530

    first commit

---
+++ src/main/java/bool/example/com/App.java
@@ -0,0 +1,14 @@
+package bool.example.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Parvez!" );
+        System.out.println( "Hello Parvez!" );
+    }
+}
